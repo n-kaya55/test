c1ceeec2f6566b53d65bba39d53d1b9bdc8da403
nesibe kaya <nesibe.kaya104@gmail.com>
2024-02-14 22:27:14 +0300

    update

---
+++ main.py
@@ -0,0 +1,23 @@
+print ("hello")
+
+#degiskenler 
+
+text ="merhaba"
+print (text)
+averagePoint =25.5
+
+print (averagePoint+5)
+
+'''
+comment
+'''
+isVerified = True 
+
+print(type(isVerified))
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
