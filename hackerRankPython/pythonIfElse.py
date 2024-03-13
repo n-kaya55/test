98a8b0423af26e6da9c92856121f25d5d90887ca
nesibe kaya <nesibe.kaya104@gmail.com>
2024-03-13 22:15:15 +0300

    update

--- hackerRankPython/pythonIfElse.py
+++ hackerRankPython/pythonIfElse.py
@@ -21,9 +21,9 @@ if __name__ == '__main__':
     if n%2!=0:
         print("Weird")
     elif n%2==0:
-        if n>2 and n<5:
+        if n>=2 and n<=5:
             print("Not Weird")
-        elif n<6 and n<20:
+        elif n>=6 and n<=20:
             print("Weird")
-        else:
+        elif n>=20:
             print("Not Weird")
