b282c0faf06d2a7bda74352381f708746de34d1a
nesibe kaya <nesibe.kaya104@gmail.com>
2024-03-13 21:48:56 +0300

    update

---
+++ calismalarim/yeniSorular2.py
@@ -0,0 +1,23 @@
+# Write a Python program to convert JSON data to Python object.
+
+import json
+
+# jsonObject='{"Name":"Nesibe", "Class":"testAutomation"}'
+# print(jsonObject)
+# pythonObject=json.loads(jsonObject)
+# print(pythonObject)
+
+# print("name: " ,pythonObject["Name"])
+# print("class: ", pythonObject["Class"])
+
+# Write a Python program to convert Python object to JSON data. 
+
+#create dict
+
+# pythonObject={
+#     "name":"Nesibe",
+#     "class":"testAutomation"
+# }
+
+# jsonObject=json.dumps(pythonObject)
+# print(jsonObject)
